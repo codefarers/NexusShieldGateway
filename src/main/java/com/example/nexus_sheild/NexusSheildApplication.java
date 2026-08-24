4047d884aed9e82d04c461ad03aef8a837bd57c5
romanusbett <bettromanus@gmail.com>
2026-08-24 21:40:00 +0300

    initial commit

---
+++ src/main/java/com/example/nexus_sheild/NexusSheildApplication.java
@@ -0,0 +1,13 @@
+package com.example.nexus_sheild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusSheildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexusSheildApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nexus_sheild/NexusSheildApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nexus_sheild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusSheildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
